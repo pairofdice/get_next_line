774f170614cec1e64b87e341280141abc7728ea0
Jaakko Saarinen <jsaarine@student.hive.fi>
2021-12-21 15:34:32 +0200

    First version

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 13:49:28 by jsaarine          #+#    #+#             */
+/*   Updated: 2021/12/21 14:12:44 by jsaarine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	get_next_line(const int fd, char **line)
+{
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
+
+	ret = read(fd, buff, BUFF_SIZE);
+	write(1, buff, 2);
+	return (ret);
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 14:13:00 by jsaarine          #+#    #+#             */
+/*   Updated: 2021/12/21 14:15:09 by jsaarine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+
+# include <fcntl.h>
+# include <unistd.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
