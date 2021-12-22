7f3694526e36d81060b4ab5f32b4b4d11da25b7e
Jaakko Saarinen <jsaarine@student.hive.fi>
2021-12-22 15:23:41 +0200

    ...

--- get_next_line.c
+++ get_next_line.c
@@ -18,22 +18,8 @@ int	get_next_line(const int fd, char **line)
 	int			ret;
 	char		buff[BUFF_SIZE + 1];
 
-	if (!fd_seen[fd])
-	{
-		// read until \n or \0
-		// if until \n save spot in fd_seen and return 1?
-		// if until \0 return 0?
-	}
-	else
-	{
+
 		// read(fd, *line, BUFF_SIZE);
-	}
+
 	return (ret);
 }
-
-	// check if fd has been seen
-		// if fd not seen, it is now
-		// or if we see a \0 before \n we're done reading, reset fd_seen
-	// find \n or \0
-		// put it in *line with \0
-		// save our spot in *line with the corresponding fd
