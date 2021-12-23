b434ec851e0688d27c8a99217c12051602b34ada
Jaakko Saarinen <pod@fea.st>
2021-12-23 17:39:55 +0200

    progress on basic structure

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 13:49:28 by jsaarine          #+#    #+#             */
-/*   Updated: 2021/12/22 12:10:32 by jsaarine         ###   ########.fr       */
+/*   Updated: 2021/12/23 17:39:20 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,24 @@ int	get_next_line(const int fd, char **line)
 	int			ret;
 	char		buff[BUFF_SIZE + 1];
 
-
-		// read(fd, *line, BUFF_SIZE);
-
+	if (!fd_seen[fd])
+	{
+		ret = 1;
+		while (ret)
+		{
+			ret = read(fd, *line, BUFF_SIZE);
+			// dynamic arrays here, maybe?
+			// read the whole file into buffer
+		}
+	}
 	return (ret);
 }
+
+	// if fd in fd_seen
+		// get fd_seen[fd] ... hmm, not needed?
+	// else
+		// read(fd, *line, BUFF_SIZE);
+	// read until '\n' or '\0'
+		// match '\n'
+		// match '\0
+		// strsep?
