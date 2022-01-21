3b4cdf8fc8c3331beed4fe14c2cb82cc32cd90b8
Jaakko Saarinen <pod@fea.st>
2022-01-21 18:09:33 +0200

    Indexing...

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 13:49:28 by jsaarine          #+#    #+#             */
-/*   Updated: 2022/01/21 17:32:51 by jsaarine         ###   ########.fr       */
+/*   Updated: 2022/01/21 18:07:49 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,11 +61,11 @@ int	get_next_line(const int fd, char **line)
 				return (-1);
 			ret = read(fd, temp_string, BUFF_SIZE);
 		}
-
 	}
-	// I'm still not inserting terminators anywhere
-	*line = temp_vec.memory[fd_seen[fd]->index];
-	fd_seen[fd]->index += ft_strlen(temp_vec.memory[fd_seen[fd]->index] + 2);
+	// I'm still not inserting terminators at newlines
+	vec_push(fd_seen[fd]->content_vec, '\0');
+	*line = &temp_vec.memory[fd_seen[fd]->index];
+	fd_seen[fd]->index += ft_strlen(temp_vec.memory[fd_seen[fd]->index] + 1);
 
 	return (0);
 }
@@ -82,7 +82,7 @@ when we haven't seen fd before:
 	init a struct with fd, index 0, vector
 		if not good, free, return -1
 	read the whole input into the vector
-		just vec_append() // TODO
+		just vec_append()
 		amount of stuff we read should be in vector.len
 	replace newlines with terminators to make each line a separate string
 	if we're not done with this input, put it in the fd_seen buffer
@@ -95,6 +95,6 @@ if we have seen this FD before
 
 update index to point past the bit we just read
 
-
+???
 
  */
