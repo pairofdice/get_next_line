597a952a1ba21cc5708e2f4d1e8cd0dfce4b6295
Jaakko Saarinen <pod@fea.st>
2022-02-14 15:08:18 +0200

    merge

--- eval_tests/get_next_line.c
+++ eval_tests/get_next_line.c
@@ -6,83 +6,88 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 13:49:28 by jsaarine          #+#    #+#             */
-/*   Updated: 2022/02/10 11:21:08 by jsaarine         ###   ########.fr       */
+/*   Updated: 2022/02/14 14:39:47 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../get_next_line.h"
 #include <stdio.h>
+
+
+void	output(t_vec *storage, char **line)
+{
+	ssize_t	len;
+
+	*line = ft_strdup(storage->memory);
+	len = ft_strlen(storage->memory);
+	if (storage->len > len + 1)
+	{
+		ft_memcpy(storage->memory,
+			&storage->memory[len + 1],
+			storage->len - len + 1);
+		storage->len -= len + 1;
+	}
+	else
+		storage->len = 0;
+}
+
+void	read_into_storage(t_vec *storage, char **line, const int fd)
+{
+	t_vec	transfer;
+	ssize_t	ret;
+	char	*hodl;
+	char	buffer[BUFF_SIZE + 1];
+
+	ret = read(fd, buffer, BUFF_SIZE);
+	while (ret > 0)
+	{
+		buffer[ret] = '\0';
+		//vec_strapp(storage, buffer);
+		vec_from(&transfer, buffer, ft_strlen(buffer), 1);
+		vec_append(storage, &transfer);
+		hodl = ft_strchr(storage->memory, '\n');
+		if (hodl)
+		{
+			while (hodl)
+			{
+				*hodl = '\0';
+				hodl = ft_strchr(storage->memory, '\n');
+			}
+			break ;
+		}
+		ret = read(fd, buffer, BUFF_SIZE);
+	}
+}
 
 int	get_next_line(const int fd, char **line)
 {
-	char			read_into[BUFF_SIZE + 1];
-	char			*hodl;
 	t_vec			*fd_seen[MAX_FD];
-	t_vec			transfer;
-	static t_vec	buffer;
-	ssize_t			ret;
-	ssize_t			len;
+	static t_vec	storage;
+	int				len;
+	char			*hodl;
 
-	if (fd_seen[fd])
+	if (fd < 0 || fd >= MAX_FD || line == NULL)
+		return (-1);
+
+	if (!fd_seen[fd])
 	{
-		 // vec_push(fd_seen[fd], "\0");
-		if (fd_seen[fd]->len == 0)
-		{
-			vec_free(fd_seen[fd]);
-			fd_seen[fd] = 0;
-			return (-1);
-		}
-		if (fd_seen[fd]->len >= ft_strlen(fd_seen[fd]->memory) + 1)
-		{
-			fd_seen[fd]->memory += ft_strlen(fd_seen[fd]->memory) + 1; // need a check for this
-			fd_seen[fd]->len = ft_strlen(fd_seen[fd]->memory) ;
-
-		}
-		else
-		{
-			ft_putendl("Do we ever get here");
-		 	vec_free(fd_seen[fd]);
-			ft_putendl("after free");
-			fd_seen[fd] = 0;
-			return (0);
-		}
-		/* if (fd_seen[fd]->len == ft_strlen(fd_seen[fd]->memory) + 1) */
+		vec_new(&storage, BUFF_SIZE * 2, 1);
+		fd_seen[fd] = &storage;
 	}
-	else
+	hodl = ft_strchr(storage.memory, '\n');
+	if (hodl)
 	{
-
-		vec_new(&buffer, BUFF_SIZE, 1);
-		fd_seen[fd] = &buffer;
+		*hodl = '\0';
+		output(&storage, line);
+		return (1);
 	}
-	ret = read(fd, read_into, BUFF_SIZE);
-	if (ret == 0)
+	read_into_storage(fd_seen[fd], line, fd);
+	if (fd_seen[fd]->len <= 0)
+	{
+		vec_free(fd_seen[fd]);
+		fd_seen[fd] = 0;
 		return (0);
-	while (ret > 0)
-	{
-		read_into[ret] = '\0';
-		vec_from(&transfer, read_into, ft_strlen(read_into), 1);
- 		vec_append(fd_seen[fd], &transfer);
- 		//vec_free(&transfer);
-		hodl = ft_strchr(read_into, '\n');
-		if (hodl)
-		{
-			*hodl = '\0';
-			//fd_seen[fd]->len = ft_strlen(fd_seen[fd]->memory  ) + 1;
-			break; // maybe push null terminator onto fd_seen[fd] here
-		}
-		ret = read(fd, read_into, BUFF_SIZE);
 	}
-
-/* 	if (fd_seen[fd] != 0 )
-	{
-		if(fd_seen[fd]->len == 0)
-		{
-			vec_free(fd_seen[fd]);
-			fd_seen[fd] = 0;
-			return (0);
-
-		}
-	} */
-	*line = ft_strdup(fd_seen[fd]->memory);
+	output(fd_seen[fd], line);
 	return (1);
 }
